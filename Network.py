771d05f7f83cbc198630b4efdeecf9ab816ffcc7
hctel <hugo.de.hepcee@gmail.com>
2024-03-28 10:25:07 +0100

    Completed client subscribing

--- Network.py
+++ Network.py
@@ -1,4 +1,5 @@
-import socket, json, time
+import socket, json
+from json.decoder import JSONDecodeError
 
 
 subscribeRequest = "{{\"request\": \"subscribe\",\"port\": {},\"name\": \"Not-A-Virus.exe\",\"matricules\": [\"22054\", \"2217\"]}}"
@@ -14,7 +15,7 @@ class Network:
         self.__socket.connect(self.__address)
         data = subscribeRequest.format(serverPort).encode("utf8")
         sentBytes = 0
-        while sentBytes == len(data):
+        while sentBytes < len(data):
             sent = self.__socket.send(data[sentBytes:])
             sentBytes+=sent
         chunks = []
@@ -22,7 +23,23 @@ class Network:
         while not finished:
             data = self.__socket.recv(1024)
             chunks.append(data)
-            finished = data == ''
-        print(''.join(chunks).decode())
+            finished = data == b''
+        try:
+            subResp = json.loads(b''.join(chunks).decode())
+            if subResp["response"] == "ok":
+                self.__subscribed = True
+            else:
+                self.__subscribed = False
+                errorCode = subResp["error"]
+                print(f"Couldn't subscribe with the GameManager: {errorCode}")
+        except JSONDecodeError:
+            print("Received subscribe response not a JSON string")
+        self.__socket.close()
         
-network = Network("127.0.0.1", 3000)
+        
+    @property
+    def isSubscribed(self):
+        return self.__subscribed
+        
+network = Network("localhost", 3000)
+print(network.isSubscribed)
