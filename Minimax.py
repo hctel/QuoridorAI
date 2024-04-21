6507986d8f3544219dc382c45341aac3cd59580f
Deuvaert Victor <22054@ecam.be>
2024-04-21 21:54:40 +0200

    add condition to prevent "crossing blockers"

--- Minimax.py
+++ Minimax.py
@@ -98,12 +98,12 @@ def getBlockers(board):
 	# need a security to prevent Pathfinder fail (no exit)
 	for y in range(1, len(board)-1, 2):
 		for x in range(0, len(board[0])-4, 2):
-			if board[y][x]==3 and board[y][x+2]==3:
+			if (board[y][x]==EMPTY_BLOCKER and board[y][x+2]==EMPTY_BLOCKER) and (board[y-1][x+1]==EMPTY_BLOCKER and board[y+1][x+1]==EMPTY_BLOCKER):
 				move = {'type':"blocker", 'position':[[y, x], [y, x+2]]} # horizontal
 				res.append(move)
 	for y in range(0, len(board)-4, 2):
 		for x in range(1, len(board[0])-1, 2):
-			if board[y][x]==3 and board[y+2][x]==3:
+			if board[y][x]==EMPTY_BLOCKER and board[y+2][x]==EMPTY_BLOCKER and (board[y+1][x-1]==EMPTY_BLOCKER and board[y+1][x+1]==EMPTY_BLOCKER):
 				move = {'type':"blocker", 'position':[[y, x], [y+2, x]]} # vertical
 				res.append(move)
 	#print("len(res) = ", len(res))
