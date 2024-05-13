8fbc9cebea4d832e9f1d5b413513d5dfb10ba162
Deuvaert Victor <22054@ecam.be>
2024-05-13 19:13:36 +0200

    Add training pools comments

--- Trainer.py
+++ Trainer.py
@@ -6,14 +6,47 @@ CUSTOM = 2
 import multiprocessing as mp
 import Main
 from random import randint, uniform
-number_of_cores = 22
+number_of_cores = 10
 start_port = 3001
 
-center_weights = [0,0,0,0,0,0]
-delta = 0.5
+center_weights = [-13,10,-1,-13,-4,-5]
+delta = 10
 mode = CUSTOM
 
-custom_weights = [[-5,5,7,13,-4,11],[-8,4,2,16,1,10],[-8,7,13,-9,7,-4],[-13,-12,-6,-1,13,-8],[-8,13,-14,5,6,11]]
+# 1st round
+#custom_weights = [[-5,5,7,13,-4,11],[-8,4,2,16,1,10],[-8,7,13,-9,7,-4],[-13,-12,-6,-1,13,-8],[-8,13,-14,5,6,11]]
+#custom_weights = [[-5,5,7,13,-4,11],[-8,4,2,16,1,10],[-13,-12,-6,-1,13,-8],[-8,13,-14,5,6,11]]
+#custom_weights = [[-8,4,2,16,1,10],[-13,-12,-6,-1,13,-8],[-8,13,-14,5,6,11]]
+#custom_weights = [[-8,4,2,16,1,10],[-13,-12,-6,-1,13,-8]]
+#custom_weights = [[-8,4,2,16,1,10]]
+
+#custom_weights = [[-8,4,2,16,1,10],[-9,5,2,15,2,11]] # good winners
+
+# 2nd round
+#custom_weights = [[-10,5,-15,-13,-5,5],[-2,6,-4,-7,13,15],[0,-14,-3,10,6,-4]]
+
+# 3nd round
+#custom_weights = [[-13,10,-1,-13,-4,-5],[6,14,-10,-9,7,-10]]
+
+# 4nd round
+#custom_weights = [[-14,-12,-11,2,-8,-14],[-5,10,-13,7,11,-15]]
+
+# 4nd round
+#custom_weights = [[-10,11,4,-7,-13,3]]
+
+# championship
+'''custom_weights = [[-8,4,2,16,1,10],[-13,-12,-6,-1,13,-8],[-8,13,-14,5,6,11],[-9,5,2,15,2,11],
+                  [-10,5,-15,-13,-5,5],[-2,6,-4,-7,13,15],[0,-14,-3,10,6,-4],
+                  [-13,10,-1,-13,-4,-5],[6,14,-10,-9,7,-10],
+                  [-14,-12,-11,2,-8,-14],[-5,10,-13,7,11,-15],
+                  [-10,11,4,-7,-13,3]]'''
+
+#custom_weights = [[-8,4,2,16,1,10],[-9,5,2,15,2,11],[-13,10,-1,-13,-4,-5]]
+#custom_weights = [[-8,4,2,16,1,10],[-9,5,2,15,2,11],[-13,10,-1,-13,-4,-5],[-8,13,-1,-6,-5,2],[-9,14,-2,-15,-14,-11],[-8,4,2,16,1,10]]
+#custom_weights = [[-8,4,2,16,1,10],[-9,5,2,15,2,11],[-13,10,-1,-13,-4,-5],[-8,4,2,16,1,10]] # -9 is better
+#custom_weights = [[-9,14,-2,-15,-14,-11],[-9,5,2,15,2,11]] # -9,5 is better
+
+custom_weights = [[-9,5,2,15,2,11],[-13,10,-1,-13,-4,-5]]
 
 def run(port, w):
     Main.train(port, w)   
@@ -34,7 +67,7 @@ if __name__ == "__main__":
         ports = 3001
         for i in range(3001, 3001+number_of_cores):
             print(i)
-            p = mp.Process(target = run, args = (i, [uniform(-delta, delta)+center_weights[0], uniform(-delta, delta)+center_weights[1],uniform(-delta, delta)+center_weights[2],uniform(-delta, delta)+center_weights[3],uniform(-delta, delta)+center_weights[4],uniform(-delta, delta)+center_weights[5]]))
+            p = mp.Process(target = run, args = (i, [randint(-delta, delta)+center_weights[0], randint(-delta, delta)+center_weights[1],randint(-delta, delta)+center_weights[2],randint(-delta, delta)+center_weights[3],randint(-delta, delta)+center_weights[4],randint(-delta, delta)+center_weights[5]]))
             p.start()
             processes.append(p)
         processes[0].join()
